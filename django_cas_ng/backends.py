0b058f74f5775cb4619d938eaca082d4aee3f1c4
Mehdi Benadda <me@mbenadda.com>
2020-03-26 16:05:15 +0100

    Use our UserMapping in the authentication backend
    Now that we have a model that enables linking Users with their
    CAS/SAML provider ids, we can use it to find existing users as they
    log in or create them along with their mapping if they do not have
    an account yet.
    
    We also removed some code that handled field dependencies on the actual
    User model as we don't want to rely on those anymore.

--- django_cas_ng/backends.py
+++ django_cas_ng/backends.py
@@ -10,26 +10,44 @@ from django.core.exceptions import ImproperlyConfigured
 from django.http import HttpRequest
 from django_cas_ng.signals import cas_user_authenticated
 
+from .models import UserMapping
 from .utils import get_cas_client
 
-__all__ = ['CASBackend']
+__all__ = ["CASBackend"]
 
 
 class CASBackend(ModelBackend):
     """CAS authentication backend"""
 
-    def authenticate(self, request: HttpRequest, ticket: str, service: str) -> Optional[User]:
+    def authenticate(
+        self, request: HttpRequest, ticket: str, service: str
+    ) -> Optional[User]:
         """
         Verifies CAS ticket and gets or creates User object
 
         :returns: [User] Authenticated User object or None if authenticate failed.
         """
+        UserModel = get_user_model()
+
         client = get_cas_client(service_url=service, request=request)
         username, attributes, pgtiou = client.verify_ticket(ticket)
-        if attributes and request:
-            request.session['attributes'] = attributes
 
-        if settings.CAS_USERNAME_ATTRIBUTE != 'uid' and settings.CAS_VERSION != 'CAS_2_SAML_1_0':
+        # Get the user from a user mapping that links a user with their identity provider id
+        try:
+            user_mapping = UserMapping.objects.get(id=username)
+        except UserMapping.DoesNotExist:
+            user_mapping = None
+
+        # Dictionary of fields to add to the user as we create or update it
+        user_kwargs = {}
+
+        if attributes and request:
+            request.session["attributes"] = attributes
+
+        if (
+            settings.CAS_USERNAME_ATTRIBUTE != "uid"
+            and settings.CAS_VERSION != "CAS_2_SAML_1_0"
+        ):
             if attributes:
                 username = attributes.get(settings.CAS_USERNAME_ATTRIBUTE)
             else:
@@ -52,72 +70,61 @@ class CASBackend(ModelBackend):
                     attributes[req_attr_name] = attributes[cas_attr_name]
                     attributes.pop(cas_attr_name)
 
-        UserModel = get_user_model()
+            if settings.CAS_APPLY_ATTRIBUTES_TO_USER:
+                # If we are receiving None for any values which cannot be NULL
+                # in the User model, set them to an empty string instead.
+                # Possibly it would be desirable to let these throw an error
+                # and push the responsibility to the CAS provider or remove
+                # them from the dictionary entirely instead. Handling these
+                # is a little ambiguous.
+                user_model_fields = UserModel._meta.fields
 
-        # Note that this could be accomplished in one try-except clause, but
-        # instead we use get_or_create when creating unknown users since it has
-        # built-in safeguards for multiple threads.
-        if settings.CAS_CREATE_USER:
-            user_kwargs = {
-                UserModel.USERNAME_FIELD: username
-            }
-            if settings.CAS_CREATE_USER_WITH_ID:
-                user_kwargs['id'] = self.get_user_id(attributes)
+                for field in user_model_fields:
+                    # Handle null -> '' conversions mentioned above
+                    if not field.null:
+                        try:
+                            if attributes[field.name] is None:
+                                user_kwargs[field.name] = ""
+                            else:
+                                user_kwargs[field.name] = attributes[field.name]
+                        except KeyError:
+                            continue
+                    # Coerce boolean strings into true booleans
+                    elif field.get_internal_type() == "BooleanField":
+                        try:
+                            boolean_value = attributes[field.name] == "True"
+                            user_kwargs[field.name] = boolean_value
+                        except KeyError:
+                            continue
+                    else:
+                        try:
+                            user_kwargs[field.name] = attributes[field.name]
+                        except KeyError:
+                            continue
 
-            user, created = UserModel._default_manager.get_or_create(**user_kwargs)
-            if created:
-                user = self.configure_user(user)
-        else:
+        if not user_mapping and not settings.CAS_CREATE_USER:
+            return None
+
+        if user_mapping:
+            # If the user already exists in our mapping, we're not creating it
             created = False
-            try:
-                if settings.CAS_LOCAL_NAME_FIELD:
-                    user_kwargs = {
-                        settings.CAS_LOCAL_NAME_FIELD: username
-
-                    }
-                    user = UserModel._default_manager.get(**user_kwargs)
-                else:
-                    user = UserModel._default_manager.get_by_natural_key(username)
-            except UserModel.DoesNotExist:
-                pass
+            user = user_mapping.user
+            if settings.CAS_APPLY_ATTRIBUTES_TO_USER:
+                for field_name, value in user_kwargs.items():
+                    setattr(user, field_name, value)
+                user.save()
+        else:
+            # Apply attributes to our user as we create it, if appropriate
+            user = UserModel.objects.create(**user_kwargs)
+            # Create a mapping to associate the user with their CAS/SMAL provider id
+            UserMapping.objects.create(id=username, user=user)
+            created = True
 
         if not self.user_can_authenticate(user):
             return None
 
         if pgtiou and settings.CAS_PROXY_CALLBACK and request:
-            request.session['pgtiou'] = pgtiou
-
-        if settings.CAS_APPLY_ATTRIBUTES_TO_USER and attributes:
-            # If we are receiving None for any values which cannot be NULL
-            # in the User model, set them to an empty string instead.
-            # Possibly it would be desirable to let these throw an error
-            # and push the responsibility to the CAS provider or remove
-            # them from the dictionary entirely instead. Handling these
-            # is a little ambiguous.
-            user_model_fields = UserModel._meta.fields
-            for field in user_model_fields:
-                # Handle null -> '' conversions mentioned above
-                if not field.null:
-                    try:
-                        if attributes[field.name] is None:
-                            attributes[field.name] = ''
-                    except KeyError:
-                        continue
-                # Coerce boolean strings into true booleans
-                if field.get_internal_type() == 'BooleanField':
-                    try:
-                        boolean_value = attributes[field.name] == 'True'
-                        attributes[field.name] = boolean_value
-                    except KeyError:
-                        continue
-
-            user.__dict__.update(attributes)
-
-            # If we are keeping a local copy of the user model we
-            # should save these attributes which have a corresponding
-            # instance in the DB.
-            if settings.CAS_CREATE_USER:
-                user.save()
+            request.session["pgtiou"] = pgtiou
 
         # send the `cas_user_authenticated` signal
         cas_user_authenticated.send(
@@ -129,30 +136,9 @@ class CASBackend(ModelBackend):
             pgtiou=pgtiou,
             ticket=ticket,
             service=service,
-            request=request
+            request=request,
         )
         return user
-
-    def get_user_id(self, attributes: Mapping[str, str]) -> str:
-        """
-        For use when CAS_CREATE_USER_WITH_ID is True. Will raise ImproperlyConfigured
-        exceptions when a user_id cannot be accessed. This is important because we
-        shouldn't create Users with automatically assigned ids if we are trying to
-        keep User primary key's in sync.
-
-        :returns: [string] user id.
-        """
-        if not attributes:
-            raise ImproperlyConfigured("CAS_CREATE_USER_WITH_ID is True, but "
-                                       "no attributes were provided")
-
-        user_id = attributes.get('id')
-
-        if not user_id:
-            raise ImproperlyConfigured("CAS_CREATE_USER_WITH_ID is True, but "
-                                       "`'id'` is not part of attributes.")
-
-        return user_id
 
     def clean_username(self, username: str) -> str:
         """
@@ -167,14 +153,15 @@ class CASBackend(ModelBackend):
         :returns: [string] The cleaned username.
         """
         username_case = settings.CAS_FORCE_CHANGE_USERNAME_CASE
-        if username_case == 'lower':
+        if username_case == "lower":
             username = username.lower()
-        elif username_case == 'upper':
+        elif username_case == "upper":
             username = username.upper()
         elif username_case is not None:
             raise ImproperlyConfigured(
                 "Invalid value for the CAS_FORCE_CHANGE_USERNAME_CASE setting. "
-                "Valid values are `'lower'`, `'upper'`, and `None`.")
+                "Valid values are `'lower'`, `'upper'`, and `None`."
+            )
         return username
 
     def configure_user(self, user: User) -> User:
@@ -190,10 +177,9 @@ class CASBackend(ModelBackend):
         """
         return user
 
-    def bad_attributes_reject(self,
-                              request: HttpRequest,
-                              username: str,
-                              attributes: Mapping[str, str]) -> bool:
+    def bad_attributes_reject(
+        self, request: HttpRequest, username: str, attributes: Mapping[str, str]
+    ) -> bool:
         """
         Rejects a user if the returned username/attributes are not OK.
 
