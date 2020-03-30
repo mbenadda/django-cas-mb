ca17aa2b69ac628dce54072ae3f0ee8a518b2530
Mehdi Benadda <me@mbenadda.com>
2020-03-30 15:03:49 +0200

    Add a migration for the UserMapping
    As we added a new `UserMapping` model to django-cas, we need to
    create a migration to actually create it in the database.

--- django_cas_ng/migrations/0001_initial.py
+++ django_cas_ng/migrations/0001_initial.py
@@ -15,26 +15,53 @@ class Migration(migrations.Migration):
 
     operations = [
         migrations.CreateModel(
-            name='ProxyGrantingTicket',
+            name="ProxyGrantingTicket",
             fields=[
-                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('session_key', models.CharField(blank=True, max_length=255, null=True)),
-                ('pgtiou', models.CharField(blank=True, max_length=255, null=True)),
-                ('pgt', models.CharField(blank=True, max_length=255, null=True)),
-                ('date', models.DateTimeField(auto_now_add=True)),
-                ('user', models.ForeignKey(blank=True, null=True, on_delete=django.db.models.deletion.CASCADE, related_name='+', to=settings.AUTH_USER_MODEL)),
+                (
+                    "id",
+                    models.AutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name="ID",
+                    ),
+                ),
+                (
+                    "session_key",
+                    models.CharField(blank=True, max_length=255, null=True),
+                ),
+                ("pgtiou", models.CharField(blank=True, max_length=255, null=True)),
+                ("pgt", models.CharField(blank=True, max_length=255, null=True)),
+                ("date", models.DateTimeField(auto_now_add=True)),
+                (
+                    "user",
+                    models.ForeignKey(
+                        blank=True,
+                        null=True,
+                        on_delete=django.db.models.deletion.CASCADE,
+                        related_name="+",
+                        to=settings.AUTH_USER_MODEL,
+                    ),
+                ),
             ],
         ),
         migrations.CreateModel(
-            name='SessionTicket',
+            name="SessionTicket",
             fields=[
-                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('session_key', models.CharField(max_length=255)),
-                ('ticket', models.CharField(max_length=255)),
+                (
+                    "id",
+                    models.AutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name="ID",
+                    ),
+                ),
+                ("session_key", models.CharField(max_length=255)),
+                ("ticket", models.CharField(max_length=255)),
             ],
         ),
         migrations.AlterUniqueTogether(
-            name='proxygrantingticket',
-            unique_together={('session_key', 'user')},
+            name="proxygrantingticket", unique_together={("session_key", "user")},
         ),
     ]
